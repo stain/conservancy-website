fb307d8cf29ed593aeed4a5fdfcb0ca05ff44f79
Bradley M. Kuhn <bkuhn@ebb.org>
2010-09-26 19:24:38 -0400

    URL and variable name fixes; added license text.

--- www/conservancy/settings.py
+++ www/conservancy/settings.py
@@ -1,13 +1,32 @@
+# Copyright 2005-2008, Software Freedom Law Center, Inc.
+# Copyright 2010, Bradley M. Kuhn
+
+# This software's license gives you freedom; you can copy, convey,
+# propagate, redistribute, modify and/or redistribute modified versions of
+# this program under the terms of the GNU Affero General Public License
+# (AGPL) as published by the Free Software Foundation (FSF), either
+# version 3 of the License, or (at your option) any later version of the
+# AGPL published by the FSF.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program in a file in the toplevel directory called
+# "AGPLv3".  If not, see <http://www.gnu.org/licenses/>.
+
 from djangocommonsettings import *
 
 SITE_ID = 2
-MEDIA_ROOT = '/var/www/external-website/conservancy/static/media/'
-MEDIA_URL = 'http://conservancy.softwarefreedom.org/media'
+MEDIA_ROOT = '/var/www/conservancy/static/media/'
+MEDIA_URL = 'http://sfconservancy.org/media'
 ROOT_URLCONF = 'conservancy.urls'
-FORCE_CANONICAL_HOSTNAME = "conservancy.softwarefreedom.org"
+FORCE_CANONICAL_HOSTNAME = "sfconservancy.org"
 
 TEMPLATE_DIRS = (
-    '/var/www/external-website/conservancy/templates',
+    '/var/www/conservancy/templates',
 )
 
 try:
