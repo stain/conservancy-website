9ec1bbe85a7627d4c838243e83d16141fc804c8b
Bradley M. Kuhn <bkuhn@ebb.org>
2010-10-04 11:27:04 -0400

    Corrected front page code and template so that blog posts appear.

--- www/conservancy/frontpage.py
+++ www/conservancy/frontpage.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from conservancy.apps.news.models import PressRelease
+from conservancy.apps.blog.models import Entry as BlogEntry
 from datetime import datetime, timedelta
 
 def view(request):
@@ -9,8 +10,10 @@ def view(request):
     """
 
     press_releases = PressRelease.objects.all().filter(pub_date__lte=datetime.now(), sites=2)[:5]
+    blog = BlogEntry.objects.all().filter(pub_date__lte=datetime.now())[:3]
 
     c = {
         'press_releases': press_releases,
+        'blog' : blog
     }
     return render_to_response("frontpage.html", c)
