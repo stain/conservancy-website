9b59b9efe3480cb2742513c2c8354d9341a6c02e
Bradley M. Kuhn <bkuhn@ebb.org>
2010-10-05 10:45:50 -0400

    Don't require trailing / on RSS feed URLs.

--- www/conservancy/urls.py
+++ www/conservancy/urls.py
@@ -5,7 +5,7 @@ handler404 = 'modpythoncustom.view404'
 
 urlpatterns = patterns('',
     (r'^$', 'conservancy.frontpage.view'),
-    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed',
+    (r'^feeds/(?P<url>.*)/?$', 'django.contrib.syndication.views.feed',
      {'feed_dict': feed_dict}),
     (r'^feeds/$', 'conservancy.feeds.view'),
     (r'^news/', include('conservancy.apps.news.urls')),
