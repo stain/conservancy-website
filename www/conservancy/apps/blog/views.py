1e396f77a39221b9cc8a4d9620ee92f657a5d547
Bradley M. Kuhn <bkuhn@ebb.org>
2015-03-04 15:24:53 -0800

    More extra_context stuff?  Put it all in there!
    Simply move the blog_entries field into extra_context and pass the whole
    darn thing along to the template.

--- www/conservancy/apps/blog/views.py
+++ www/conservancy/apps/blog/views.py
@@ -80,9 +80,9 @@ def custom_index(request, queryset, *args, **kwargs):
         # If page is out of range (e.g. 9999), deliver last page of results.
         blog_entires = paginator.page(paginator.num_pages)
 
-    return render_to_response('blog/entry_list.html',
-                              {"blog_entries": blog_entries, "date_list" : extra_context['date_list'],
-                               "authors" : extra_context['authors'], "tags" : extra_context['tags'] })
+    extra_content['blog_entries'] = blog_entries
+
+    return render_to_response('blog/entry_list.html', extra_content)
 
     callable = BlogListView.as_view(**kwargs)
     return callable(request)
