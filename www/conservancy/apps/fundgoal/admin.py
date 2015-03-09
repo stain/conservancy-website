db4a82f43268817cf6eced4a7bea6fe97b934e4f
Bradley M. Kuhn <bkuhn@ebb.org>
2015-03-08 23:00:32 -0700

    Correct goal amount for admin display.

--- www/conservancy/apps/fundgoal/admin.py
+++ www/conservancy/apps/fundgoal/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from conservancy.apps.fundgoal.models import FundraisingGoal
 
 class FundraisingGoalAdmin(admin.ModelAdmin):
-    list_display = ('display_name', 'display_until_date')
+    list_display = ('fundraiser_code_name', 'fundraiser_goal_amount')
 
 admin.site.register(FundraisingGoal, FundraisingGoalAdmin)
